0b0b7f8c07078ec6d6cc1cd39b707144ef348b55
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-19 22:01:35 -0300

    refactor: first test

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button"
+import { useTheme } from "./context/theme-context"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
+  const { setTheme, } = useTheme()
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-blue-200 text-black dark:bg-blue-950 dark:text-blue-400 h-screen">
+      <p>Hello tailwind</p>
+
+      <div className="flex gap-4 text-dark dark:text-white">
+        <Button className="hover:cursor-pointer" onClick={() => setTheme('light')}>Light</Button>
+        <Button className="hover:cursor-pointer" onClick={() => setTheme('dark')}>Dark</Button>
+        <Button className="hover:cursor-pointer" onClick={() => setTheme('system')}>System</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
