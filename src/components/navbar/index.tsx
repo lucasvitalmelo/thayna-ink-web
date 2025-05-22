99a8806c341f0108996293cd27a651052c454958
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-22 00:46:36 -0300

    refactor: Menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,55 +1,13 @@
-import { Popover, PopoverTrigger, PopoverContent } from "@radix-ui/react-popover";
-import { AlignJustify, Moon, Settings, Sun } from "lucide-react";
-import { Button } from "../ui/button";
-import { useTheme } from "@/context/theme-context";
+import { Menu } from "./menu";
 
 export function Navbar() {
-  const { setTheme, theme } = useTheme()
   return (
     <nav className="flex w-full h-15 items-center justify-center border-b-2 border-gray-700">
       <div className="flex justify-between max-w-7xl w-full mx-5">
         <span>Thayna Vieira</span>
 
+        <Menu />
 
-        <Popover>
-          <PopoverTrigger className="cursor-pointer hover:opacity-85 transition-all">
-            <AlignJustify />
-          </PopoverTrigger>
-
-
-          <PopoverContent align="center" side="bottom" sideOffset={10}>
-            <div className="flex gap-2 bg-gray-700 p-4 rounded">
-
-              <span></span>
-              <Button
-                size={"icon"}
-                variant={"outline"}
-                disabled={'light' === theme}
-                className="hover:cursor-pointer"
-                onClick={() => setTheme('light')}>
-                <Sun />
-              </Button>
-
-              <Button
-                size={"icon"}
-                variant={"outline"}
-                disabled={'dark' === theme}
-                className="hover:cursor-pointer"
-                onClick={() => setTheme('dark')}>
-                <Moon />
-              </Button>
-
-              <Button
-                size={"icon"}
-                variant={"outline"}
-                disabled={'system' === theme}
-                className="hover:cursor-pointer"
-                onClick={() => setTheme('system')}>
-                <Settings />
-              </Button>
-            </div>
-          </PopoverContent>
-        </Popover>
       </div>
     </nav>)
 }
