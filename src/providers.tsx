dbc2cd2062ed25de94a318c5641f447003133236
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-21 21:13:27 -0300

    feat: Add `devtools` and `dialog`

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react"
 import { ThemeProvider } from "./context/theme-provider"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export function Provivers({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient()
@@ -8,6 +9,7 @@ export function Provivers({ children }: { children: ReactNode }) {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
         {children}
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ThemeProvider>
   )
