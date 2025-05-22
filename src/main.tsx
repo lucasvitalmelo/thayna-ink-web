36e9621a2618966c3c2ee7a7cb757632229efbcc
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-22 00:50:18 -0300

    refactor: Remove `App`

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provivers } from './providers.tsx'
-import { App } from './App.tsx'
 
 import './global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provivers >
-      <App />
-
-    </Provivers>
-
+    <Provivers />
   </StrictMode>,
 )
