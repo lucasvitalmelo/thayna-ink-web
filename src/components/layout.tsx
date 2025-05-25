8e12b52f56657c39b51f3d10c914fded24aa27fe
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-24 21:52:51 -0300

    refactor: css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,8 +6,12 @@ const Layout = () => {
   return (
     <div>
       <Navbar />
-      <main className='bg-blue-200 text-black dark:bg-gray-950 dark:text-gray-100 pt-5'>
-        <Outlet />
+      <main className='bg-blue-200 text-black dark:bg-gray-950 dark:text-gray-100'>
+        <section className="flex w-full items-center justify-center overflow-auto">
+          <div className="flex flex-col max-w-7xl w-full mx-5 pt-4 h-[calc(100vh-60px)]">
+            <Outlet />
+          </div>
+        </section>
       </main>
     </div>
   );
