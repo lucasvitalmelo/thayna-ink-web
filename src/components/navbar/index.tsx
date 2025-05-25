d9e40db74f7392845d5f62084ea5f96564ce5f96
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-24 21:51:16 -0300

    refactor: Remove menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,46 @@
-import { Menu } from "./menu";
+import { NavLink } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useTheme } from "@/context/theme-context";
+import { Moon, Settings, Sun } from "lucide-react";
 
 export function Navbar() {
+  const { setTheme, theme } = useTheme()
+
+  const currentTheme = theme === 'dark' ? 'light' : 'dark'
+
+  console.log(currentTheme)
+
   return (
     <nav className="flex w-full h-15 items-center justify-center border-b-2 border-gray-700">
-      <div className="flex justify-between max-w-7xl w-full mx-5">
-        <span>Thayna Vieira</span>
+      <div className="flex items-center max-w-7xl w-full mx-5">
+        <h1 className="font-bold mr-4">Thayna Vieira</h1>
+        <NavLink to={'/'}>
+          <Button
+            size={"sm"}
+            variant={"link"}>
+            Orders
+          </Button>
+        </NavLink>
+        <div className="ml-auto flex items-center gap-2">
 
-        <Menu />
-
+          <NavLink to='/settings' >
+            <Button
+              variant={"outline"}
+              size={"sm"}
+            >
+              <Settings />
+              settings
+            </Button>
+          </NavLink>
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            onClick={() => setTheme(currentTheme)}
+          >
+            {currentTheme === 'dark' ? <Moon /> : <Sun />}
+          </Button>
+        </div>
+        {/* <Menu /> */}
       </div>
     </nav>)
 }
