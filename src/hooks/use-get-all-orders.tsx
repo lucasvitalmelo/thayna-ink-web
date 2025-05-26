07711fed7b9a4b676c1f976382e8f2d6527ea162
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-25 23:56:14 -0300

    refactor: export key and change file name

--- src/hooks/use-get-all-orders.tsx
+++ src/hooks/use-get-all-orders.tsx
@@ -1,9 +1,11 @@
 import { getAllOrders } from "@/services/modules/order/get-all-orders";
 import { useQuery } from "@tanstack/react-query";
 
+export const ALL_ORDERS_KEY = "ALL_ORDERS"
+
 export function useGetAllOrders() {
   const query = useQuery({
-    queryKey: ["ALL_ORDERS"],
+    queryKey: [ALL_ORDERS_KEY],
     queryFn: getAllOrders
   })
 
