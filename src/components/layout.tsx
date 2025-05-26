15f8d7f2dba03f95a0324a6a37848a12003b6485
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-26 00:13:26 -0300

    refactor: remove styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div>
       <Navbar />
-      <main className='bg-blue-200 text-black dark:bg-gray-950 dark:text-gray-100'>
+      <main>
         <section className="flex w-full items-center justify-center overflow-auto">
           <div className="flex flex-col max-w-7xl w-full mx-5 pt-4 h-[calc(100vh-60px)]">
             <Outlet />
