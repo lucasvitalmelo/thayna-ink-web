fa17c41a355e4735b4618baad1e78d3e7d3508a2
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-26 00:16:50 -0300

    refactor: remove `useGetAllOrders`

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,39 +1,17 @@
-import { Order } from "@/components/order"
-import { OrderFormDialog } from "@/components/new-order-form-dialog"
-import { LoaderCircle } from "lucide-react"
+import { TypographyH2 } from "@/utils/typography/typography"
+import { OrderTable } from "./order-table"
+import { OrderFormDialog } from "./order-form-dialog"
 
-import { useGetAllOrders } from "@/hooks/get-all-orders"
 
 export function Orders() {
-  const { data: orders, isLoading } = useGetAllOrders()
   return (
-    <div>
-      <section className="flex w-full items-center justify-center overflow-auto">
-        <div className="flex flex-col gap-2 max-w-7xl w-full items-end mx-5 h-[calc(100vh-100px)]">
-          <OrderFormDialog />
-          {isLoading &&
-            <div className="flex justify-center w-full"
-            >
-              <LoaderCircle size={16} className="animate-spin" />
-            </div>
-          }
+    <div className="flex flex-col gap-3">
+      <div className="flex ">
 
-          {
-            orders?.map(order =>
-              <>
-                <Order
-                  {...order}
-                />
-                <Order
-                  {...order}
-                />
-              </>
-            )
-          }
-        </div>
-      </section>
-
-
+        <TypographyH2 border={false} text="Orders" />
+        <OrderFormDialog />
+      </div>
+      <OrderTable />
     </div>
   )
 }
