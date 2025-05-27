759b971b13793752045dd2f184b2f35c9b147b5e
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-27 19:43:55 -0300

    feat: Add `CustomerInfo`

---
+++ src/pages/orders/order-details-dialog/customer-info/index.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button"
+import { TypographyMuted } from "@/utils/typography/typography"
+import { Mail, Phone } from "lucide-react"
+
+type CustomerInfoProps = {
+  customer: {
+    name: string
+    email: string
+    phone: string
+  }
+}
+
+export function CustomerInfo({ customer }: CustomerInfoProps) {
+  return (
+    <>
+      <TypographyMuted text="Customer information" />
+      <span className="flex gap-2 items-center">
+        Name: {customer.name}
+      </span>
+
+      <span className="flex gap-2 items-center">
+        <Phone size={18} />
+        {customer.phone}
+        <Button variant="secondary" className="ml-2" size="xs">WhatsApp</Button>
+      </span>
+      <span className="flex gap-2 items-center">
+        <Mail size={18} />
+        {customer.email}
+      </span>
+    </>
+  )
+}
