77366fcb7fe87cccf56bed837d30389341535324
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-27 20:02:49 -0300

    feat: Add `TattooDetails`

---
+++ src/pages/orders/order-details-dialog/tattoo-details/index.tsx
@@ -0,0 +1,24 @@
+import { TypographyMuted } from "@/utils/typography/typography"
+import { BicepsFlexed, Ruler } from "lucide-react"
+
+type TattooDetailsProps = {
+  location: string
+  width: number
+  height: number
+}
+
+export function TattooDetails({ location, width, height }: TattooDetailsProps) {
+  return (
+    <div className="flex flex-col gap-1">
+      <TypographyMuted text="Tattoo details" />
+      <span className="flex gap-2 items-center">
+        <BicepsFlexed size={18} />
+        {location}
+      </span>
+      <span className="flex gap-2 items-center">
+        <Ruler size={18} />
+        {`${width}cm x ${height}cm`}
+      </span>
+    </div>
+  )
+}
