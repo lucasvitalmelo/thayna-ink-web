3559d5bd67058017e2acd3bb2735b38c8dd4e2d0
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-27 01:53:17 -0300

    feat: Add `OrderDatails`

---
+++ src/types/order-details.ts
@@ -0,0 +1,18 @@
+import type { Customer } from "./customer"
+import type { Payment } from "./payment"
+
+export type OrderDatails = {
+  id: number
+  customer: Customer
+  scheduledDate: Date | null
+  tattooType: string
+  bodyLocation: string
+  payments: Payment[]
+  tags: string[]
+  paid: number
+  price: number
+  phone: string
+  description: string | null
+  width: number
+  height: number
+}
