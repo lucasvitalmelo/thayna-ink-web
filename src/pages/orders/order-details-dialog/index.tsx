c811a7b654e000fc91b9b15f346c0797011440e2
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-28 01:05:22 -0300

    feat: Add `order-table`

---
+++ src/pages/orders/order-details-dialog/index.tsx
@@ -0,0 +1,76 @@
+import type { Dispatch, SetStateAction } from "react"
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { LoaderCircle } from "lucide-react"
+import { useGetUniqueOrder } from "@/hooks/use-get-unique-order"
+
+import { CustomerInfo } from "./customer-info"
+import { ScheduleForm } from "./schedule-form"
+import { TattooDetails } from "./tattoo-details"
+import { DescriptionForm } from "./description-form"
+import { PaymentsForm } from "./payments-form"
+import { Tags } from "./tags"
+import { StatusSwitch } from "./status-switch"
+
+type OrderDetailsProps = {
+  orderId: number
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export function OrderDetailsDialog({ orderId, isOpen, setIsOpen }: OrderDetailsProps) {
+  const { data: order, isLoading } = useGetUniqueOrder({ orderId, isOpenDialog: isOpen })
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent>
+        <DialogHeader className="border-b pb-2">
+          <DialogTitle>Order details</DialogTitle>
+          <DialogDescription>View, edit, and add information related to an order.</DialogDescription>
+        </DialogHeader>
+
+
+        {isLoading ? (
+          <div className="mx-auto">
+            <LoaderCircle size={16} className="animate-spin" />
+          </div>
+        ) : order && (
+          <>
+            <div className="flex flex-col gap-1.5">
+              <div className="flex">
+                <CustomerInfo customer={order.customer} />
+
+                <div className="ml-auto">
+                  <StatusSwitch
+                    orderId={order.id}
+                    status={order.status}
+                  />
+                </div>
+
+              </div>
+
+              <ScheduleForm date={order.scheduledDate} orderId={order.id} />
+
+            </div>
+
+            <TattooDetails
+              location={order.bodyLocation}
+              width={order.width}
+              height={order.height}
+            />
+
+            <Tags tags={order.tags} orderId={order.id} />
+
+            <DescriptionForm
+              orderId={order.id}
+              description={order.description}
+            />
+            <PaymentsForm orderId={order.id} />
+
+            {/* todo: payment table */}
+          </>
+        )}
+      </DialogContent>
+    </Dialog>
+  )
+}
