6f49aabd89354f395aedf7f4ac2444f423c637a4
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-05-27 23:28:54 -0300

    feat: formatDate utils

---
+++ src/utils/formatDate.tsx
@@ -0,0 +1,14 @@
+
+export function formatDateTime(dateTime: Date) {
+
+  const date = new Date(dateTime)
+
+  return new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  }).format(date)
+}
